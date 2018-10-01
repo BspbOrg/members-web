1df61989b9d8e27c60cf52f7530d8eef4e967a2a
Geno Roupsky <geno@roupsky.name>
2018-10-01 13:16:13 +0300

    Добавени видове плащане

--- src/scripts/nomenclatures/plain/paymentType.js
+++ src/scripts/nomenclatures/plain/paymentType.js
@@ -2,5 +2,7 @@ module.exports = {
   card: 'PAYMENT_TYPE_CARD',
   epay: 'PAYMENT_TYPE_EPAY',
   bank: 'PAYMENT_TYPE_BANK',
-  office: 'PAYMENT_TYPE_OFFICE'
+  office: 'PAYMENT_TYPE_OFFICE',
+  action: 'PAYMENT_TYPE_ACTION',
+  post: 'PAYMENT_TYPE_POST'
 }
