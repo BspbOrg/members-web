d5051fba179e31fe482398d6ebcf1dd687d7ecb3
Geno Roupsky <geno@roupsky.name>
2018-10-02 08:56:27 +0300

    Reset params on sidebar navigation

--- src/scripts/directives/sidebar.js
+++ src/scripts/directives/sidebar.js
@@ -2,8 +2,14 @@ require('../app').directive('sidebar', /* @ngInject */function () {
   return {
     templateUrl: '/views/_sidebar.html',
     scope: {},
-    controller: /* @ngInject */function ($state) {
+    controller: /* @ngInject */function ($state, $stateParams) {
       this.isImportCollapsed = !$state.includes('auth.import')
+      this.resetParams = function () {
+        return Object.keys($stateParams).reduce(function (map, key) {
+          map[key] = undefined
+          return map
+        }, {})
+      }
     },
     controllerAs: '$ctrl'
   }
