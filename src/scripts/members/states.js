1ae41918108ad1eb1f8caf0f00a292182f217e74
Daniel Petrov <dani@code6.ninja>
2018-10-11 10:38:20 +0300

    support filter by category and expired membership

--- src/scripts/members/states.js
+++ src/scripts/members/states.js
@@ -1,7 +1,7 @@
 module.exports = /* @ngInject */function ($stateProvider) {
   $stateProvider
     .state('auth.members', {
-      url: '/members?q',
+      url: '/members?q&category&expiredMembership',
       views: {
         'content': {
           templateUrl: '/views/members/list.html',
